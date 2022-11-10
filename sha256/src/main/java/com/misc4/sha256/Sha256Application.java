ad1306233092aad6b01bcba505637e18ea4efa33
prosenjit005 <117080260+MyItWorldPro@users.noreply.github.com>
2022-11-10 12:35:53 +0530

    initial commit

---
+++ sha256/src/main/java/com/misc4/sha256/Sha256Application.java
@@ -0,0 +1,13 @@
+package com.misc4.sha256;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sha256Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sha256Application.class, args);
+	}
+
+}
---
+++ sha256/src/test/java/com/misc4/sha256/Sha256ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misc4.sha256;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sha256ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
