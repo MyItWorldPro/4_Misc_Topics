59ebf6a1f70e2f4375183017d5dead68fba25cc5
prosenjit005 <117080260+MyItWorldPro@users.noreply.github.com>
2022-11-13 10:54:36 +0530

    initial commit

---
+++ WhatsAppTwilio/src/test/java/com/misc/twilio/WhatsAppTwilioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misc.twilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppTwilioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
