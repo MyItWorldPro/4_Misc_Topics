85449226373479e8274790b9a1bd594db475ef2f
prosenjit005 <117080260+MyItWorldPro@users.noreply.github.com>
2022-12-13 11:22:14 +0530

    implemented the Drools rules engine using drl file

--- Misc1/DroolsSpringBoot/src/main/java/com/misc1/DroolsSpringBoot/controller/DroolsController.java
+++ Misc1/DroolsSpringBoot/src/main/java/com/misc1/DroolsSpringBoot/controller/DroolsController.java
@@ -1,6 +1,8 @@
 package com.misc1.DroolsSpringBoot.controller;
 
 import com.misc1.DroolsSpringBoot.dto.CarDetailsDto;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/drools")
 public class DroolsController {
+
+    @Autowired
+    private KieSession kieSession;
 
     @PostMapping(value = "traditionalWay")
     public ResponseEntity<CarDetailsDto> traditionalWay(
@@ -30,6 +35,9 @@ public class DroolsController {
     @PostMapping(value = "droolsEngine")
     public ResponseEntity<CarDetailsDto> droolsEngine(
             @RequestBody CarDetailsDto carDetailsDto) {
+
+        kieSession.insert(carDetailsDto);
+        kieSession.fireAllRules();
 
         return new ResponseEntity<>(carDetailsDto, HttpStatus.OK);
     }
--- Misc1/DroolsSpringBoot/src/test/java/com/misc1/DroolsSpringBoot/DroolsSpringBootApplicationTests.java
+++ Misc1/DroolsSpringBoot/src/test/java/com/misc1/DroolsSpringBoot/DroolsSpringBootApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DroolsSpringBootApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
