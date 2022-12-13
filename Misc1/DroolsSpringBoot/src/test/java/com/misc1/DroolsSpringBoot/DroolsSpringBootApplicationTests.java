8f62687b24fb368861bd69a2d2873ad159128614
prosenjit005 <117080260+MyItWorldPro@users.noreply.github.com>
2022-12-13 09:55:12 +0530

    initial commit

---
+++ Misc1/DroolsSpringBoot/src/test/java/com/misc1/DroolsSpringBoot/DroolsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misc1.DroolsSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
