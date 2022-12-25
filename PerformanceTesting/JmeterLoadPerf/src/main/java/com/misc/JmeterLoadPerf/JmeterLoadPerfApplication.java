3a3fd997e14454d8ba9375d17dfcabac37c8977d
prosenjit005 <117080260+MyItWorldPro@users.noreply.github.com>
2022-12-25 20:35:34 +0530

    initial commit

---
+++ PerformanceTesting/JmeterLoadPerf/src/main/java/com/misc/JmeterLoadPerf/JmeterLoadPerfApplication.java
@@ -0,0 +1,13 @@
+package com.misc.JmeterLoadPerf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeterLoadPerfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmeterLoadPerfApplication.class, args);
+	}
+
+}
---
+++ PerformanceTesting/JmeterLoadPerf/src/test/java/com/misc/JmeterLoadPerf/JmeterLoadPerfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misc.JmeterLoadPerf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmeterLoadPerfApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
